684b58b3a672e3341385a78699ea3435a662b8b7
unknown <aamir.k7199@gmail.com>
2024-10-29 18:33:49 +0530

    update

---
+++ navbar.php
@@ -0,0 +1,36 @@
+<nav class="navbar navbar-light sticky-top bg-light justify-content-between subMenu">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <a class="navbar-brand">LOGO</a>
+
+            </div>
+            <div class="col">
+                <ul class="navbar-nav mr-auto mt-2 mt-lg-0 navb">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">PRODUCTS</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">TECHNOLOGY</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">COMPANY</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">CLIENTS</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">NEWSROOM</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">CONTACT US</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">BLOG</a>
+                        </li>
+                        
+                </ul>
+                    
+            </div>
+        </div>
+</nav>
