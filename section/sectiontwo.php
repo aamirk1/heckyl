fd4a37dc2af8a6a75fa9883fd08eebdf6a19ba67
unknown <aamir.k7199@gmail.com>
2024-10-30 17:37:30 +0530

    update

--- section/sectiontwo.php
+++ section/sectiontwo.php
@@ -1,3 +1,13 @@
-<section style="background-color: #0097B2;">
-    
+<section style="background-color: #fff;">
+    <div class="container mt-5 p-5">
+        <p class="fs-3 mt-5" style="color:#0a1a44">About Heckyl</p>
+        <hr style="color:#a9703b;width: 10%;height: 10px;">
+        <p class="mt-4 text-dark" style="
+    width: 40%;
+    text-align: justify;
+">
+            Heckyl aims to be a global leader in the space of information analytics for worldwide financial markets. We
+            strive to raise the bar with everything that we offer and shall continue to do so till hell freezes over.
+        </p>
+    </div>
 </section>
